1d3418258a73081faa510ea10f6a26f4a18dc1f1
fra <fra@movida-mail.com>
2025-08-19 11:41:27 +0200

    WIP cylinder.

--- app/main.lua
+++ app/main.lua
@@ -19,8 +19,9 @@ local scene = hg.Scene()
 hg.LoadSceneFromAssets("main.scn", scene, res, hg.GetForwardPipelineInfo())
 
 local cylinder_node = scene:GetNode("cylinder")
+local camera_node = scene:GetNode("Camera")
+
 local cylinder_material = cylinder_node:GetObject():GetMaterial(0)
-local camera = scene:GetNode("Camera")
 
 -- main loop
 frame = 0
@@ -31,14 +32,10 @@ while not hg.ReadKeyboard():Key(hg.K_Escape) and hg.IsWindowOpen(win) do
     -- world matrix of the mesh you're drawing (the volumetric cylinder)
     local M = cylinder_node:GetTransform():GetWorld()
     local _, invM = hg.Inverse(M) -- InverseFast is fine too
-    local camWS = hg.GetT(camera:GetTransform():GetWorld())
+    local camWS = hg.GetT(camera_node:GetTransform():GetWorld())
     local camOBJ = invM * camWS -- point world -> object
 
     hg.SetMaterialValue(cylinder_material, 'uCamObj', camOBJ)
-
-    -- -- set your cylinder dimensions in *object space*
-    -- -- if the mesh has radius Robj and height Hobj in OBJ space:
-    -- bgfx.setUniform(uCylDims, {Robj, yminOBJ, ymaxOBJ, edgeWidth})
 
 	trs = scene:GetNode('cylinder'):GetTransform()
 	trs:SetRot(trs:GetRot() + hg.Vec3(hg.Deg(5) * hg.time_to_sec_f(dt), hg.Deg(10) * hg.time_to_sec_f(dt), 0))
